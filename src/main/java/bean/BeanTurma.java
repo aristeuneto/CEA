9769524544dcde1e389735851f4b2e7a1dc96b60
aristeuneto <neto_saj10@hotmail.com>
2020-05-21 22:13:59 -0300

    Tela de cadastro de aluno!

---
+++ src/main/java/bean/BeanTurma.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import DAO.DaoTurma;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import model.ModelTurma;
+
+/**
+ *
+ * @author neto_
+ */
+@ManagedBean
+@SessionScoped
+public class BeanTurma {
+
+    private ModelTurma modelTurma = new ModelTurma();
+
+    public BeanTurma() {
+    }
+
+    public void salvarTurma(){
+        DaoTurma daoTurma = new DaoTurma();
+        daoTurma.salvarAluno(modelTurma);
+    
+    }
+    
+    public ModelTurma getModelTurma() {
+        return modelTurma;
+    }
+
+    public void setModelTurma(ModelTurma modelTurma) {
+        this.modelTurma = modelTurma;
+    }
+
+}
